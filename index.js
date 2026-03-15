db8ed0849ddbb7eed6318c2bcaefdccd9bd9b224
samadina268 <abubakarsamad26@gmail.com>
2026-03-15 16:05:42 +0100

    changed db settings

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const mongoose = require("mongoose")
 const register = require("./routes/auth/register")
 const signin = require("./routes/auth/signIn")
 const config = require("config")
-const db = process.env.DB_URL
+const db = config.get("DB_URL")
 
 app.use(express.json())
 app.use("/home", register)
 app.use("/home", signin)
 
 
-mongoose.connect(`${db}`)
+mongoose.connect(db)
 .then(() => console.log("connected to mongoose db ..."))
 .catch(err => console.log("mongodb connection error", err))
-console.log("process:", process.env.NODE_ENV)
-console.log("db", db)
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`listening to PORT ${PORT}`))
