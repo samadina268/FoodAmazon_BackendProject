8d581003651ce8cf529944daeb9829b11a03240c
samadina268 <abubakarsamad26@gmail.com>
2026-03-18 00:39:26 +0100

    made changes

--- models/bulk.js
+++ models/bulk.js
@@ -20,4 +20,4 @@ function validatebulkproduct(data) {
 
 const product = mongoose.model("bulkProduct" , bulkSchema)
 
-module.exports = {product, val}
+module.exports = {product, validatebulkproduct}
